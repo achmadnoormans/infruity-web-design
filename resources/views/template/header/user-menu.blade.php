eaf5b31672dae9b26d9c051c91e19ae4a331b5c9
DESKTOP-OVTLN43\USER <irsyad7798@gmail.com>
2025-05-01 19:45:19 +0700

    fix(Migration):fix some migration

--- resources/views/template/header/user-menu.blade.php
+++ resources/views/template/header/user-menu.blade.php
@@ -19,10 +19,10 @@
                 <!--end::Avatar-->
                 <!--begin::Username-->
                 <div class="d-flex flex-column">
-                    <div class="fw-bold d-flex align-items-center fs-5">Noorman Setiawan
+                    <div class="fw-bold d-flex align-items-center fs-5">{{ Auth::user()->nm_user }}
                         <span class="badge badge-light-success fw-bold fs-8 px-2 py-1 ms-2">Pro</span>
                     </div>
-                    <a href="#" class="fw-semibold text-muted text-hover-primary fs-7">noorman@gmail.com</a>
+                    <a href="#" class="fw-semibold text-muted text-hover-primary fs-7">{{ Auth::user()->email }}</a>
                 </div>
                 <!--end::Username-->
             </div>
