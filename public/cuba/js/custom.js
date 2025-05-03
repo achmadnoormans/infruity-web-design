c72295a9f1877051fe67423ea3fa4766ef8095cd
DESKTOP-OVTLN43\USER <irsyad7798@gmail.com>
2025-05-03 10:45:14 +0700

    feat(UI): remove cuba

--- public/cuba/js/custom.js
+++
@@ -1,8 +0,0 @@
-
-
-$("form").submit(function () {
-    $(this).find(":submit").attr('disabled', 'disabled');
-    $(this).find(":submit").html(
-        `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...`
-    );
-});
