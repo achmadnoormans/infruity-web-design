6cfdb5fb0652268965309698c4accb14bf0031f0
DESKTOP-OVTLN43\USER <irsyad7798@gmail.com>
2025-05-04 22:32:44 +0700

    fix(Footer): change name to Infruity

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -4,20 +4,20 @@
     <div class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">
         <!--begin::Copyright-->
         <div class="text-gray-900 order-2 order-md-1">
-            <span class="text-muted fw-semibold me-1">2024&copy;</span>
-            <a href="https://keenthemes.com" target="_blank" class="text-gray-800 text-hover-primary">Keenthemes</a>
+            <span class="text-muted fw-semibold me-1">{{ date('Y') }}&copy;</span>
+            <a href="{{ url('/') }}" target="_blank" class="text-gray-800 text-hover-primary">Infruity</a>
         </div>
         <!--end::Copyright-->
         <!--begin::Menu-->
         <ul class="menu menu-gray-600 menu-hover-primary fw-semibold order-1">
             <li class="menu-item">
-                <a href="https://keenthemes.com" target="_blank" class="menu-link px-2">About</a>
+                <a href="{{ url('/') }}" target="_blank" class="menu-link px-2">About</a>
             </li>
             <li class="menu-item">
-                <a href="https://devs.keenthemes.com" target="_blank" class="menu-link px-2">Support</a>
+                <a href="{{ url('/') }}" target="_blank" class="menu-link px-2">Support</a>
             </li>
             <li class="menu-item">
-                <a href="https://1.envato.market/EA4JP" target="_blank" class="menu-link px-2">Purchase</a>
+                <a href="{{ url('/') }}" target="_blank" class="menu-link px-2">Purchase</a>
             </li>
         </ul>
         <!--end::Menu-->
