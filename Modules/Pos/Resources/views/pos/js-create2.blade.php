9d53a5f906f4c50ff10ec6a2b91b2cdf81eac91e
DESKTOP-71DD6D5\PEMKOTSBY <irsyad7798@gmail.com>
2025-08-13 13:01:04 +0700

    fx

--- Modules/Pos/Resources/views/pos/js-create2.blade.php
+++ Modules/Pos/Resources/views/pos/js-create2.blade.php
@@ -124,6 +124,13 @@
                     qty: 1
                 },
 
+                addParcel: {
+                    id: null,
+                    name: '',
+                    harga: 0,
+                    qty: 1
+                },
+
                 // Animation states
                 badgeAnimation: false,
                 priceAnimation: false,
@@ -931,16 +938,11 @@
                         modal.show();
 
                         // Init select2
-                        $('#select_gift').select2({
+                        $('#select_kemasan').select2({
                             placeholder: 'Pilih produk',
-                            language: {
-                                errorLoading: function() {
-                                    return "Tidak ada Hadiah yang tersedia.";
-                                }
-                            },
                             dropdownParent: $('#parcelModal'),
                             ajax: {
-                                url: '/tier/get-gift/' + $('#tier_id').val(), // ganti sesuai route
+                                url: '/ajax/listProduct', // ganti sesuai route
                                 dataType: 'json',
                                 delay: 250,
                                 processResults: data => ({
@@ -972,7 +974,157 @@
                     const modal = bootstrap.Modal.getInstance(document.getElementById('parcelModal'));
                     if (modal) modal.hide();
                 },
+
+                saveParcelToCart(index) {
+                    let item = this.parcels[index];
+                    console.log('Saving parcel to cart:', item);
+                    if (!item.product) {
+                        alert('Pilih parcel terlebih dahulu');
+                        return;
+                    }
+
+                    // Akses posApp dari luar
+                    let posAppInstance = document.querySelector('[x-ref=posApp]').__x.$data;
+
+                    posAppInstance.cart.push({
+                        id: item.product,
+                        name: item.name,
+                        qty: item.qty,
+                        budget: item.price
+                    });
+
+                    this.removeParcel(index);
+                },
+
+
             }
         }
+
+        function parcelForm() {
+            return {
+                parcels: [],
+                totalAll: 0,
+                cart: [],
+                qtyParcel: 1,
+                budgetParcel: '',
+                get totalAll() {
+                    // kalau "Harga Jual" = budget per item, cukup jumlahkan price
+                    return this.parcels.reduce((sum, p) => sum + (Number(p.price) || 0), 0);
+
+                    // kalau maunya total = qty * hargaAsli per item, pakai ini:
+                    // return this.parcels.reduce((sum, p) => sum + (Number(p.qty||0) * Number(p.priceAwal||0)), 0);
+                },
+                addParcel() {
+                    this.parcels.push({
+                        product: '',
+                        name: '',
+                        unit: '',
+                        priceAwal: 0,
+                        qty: 1,
+                        price: 0,
+                        priceFormatted: '',
+                        hpp: 0
+                    });
+                    this.$nextTick(() => {
+                        this.initSelect2();
+                    });
+                },
+                removeParcel(index) {
+                    this.parcels.splice(index, 1);
+                },
+                updatePrice(index) {
+                    let raw = (this.parcels[index].priceFormatted || '').replace(/\D/g, '');
+                    let hargaJualBaru = parseInt(raw);
+                    if (isNaN(hargaJualBaru)) hargaJualBaru = 0;
+
+                    this.parcels[index].price = hargaJualBaru;
+                    this.parcels[index].priceFormatted = this.formatRupiah(hargaJualBaru);
+
+                    let hargaAsli = parseInt(this.parcels[index].priceAwal || 0);
+                    if (hargaAsli > 0) {
+                        let qtyBaru = hargaJualBaru / hargaAsli;
+                        this.parcels[index].qty = parseFloat(qtyBaru.toFixed(2));
+                    }
+                },
+                updateFromQty(index) {
+                    let hargaAsli = parseInt(this.parcels[index].priceAwal || 0);
+                    if (hargaAsli > 0) {
+                        let hargaBaru = this.parcels[index].qty * hargaAsli;
+                        this.parcels[index].price = hargaBaru;
+                        this.parcels[index].priceFormatted = this.formatRupiah(hargaBaru);
+                    }
+                },
+
+                formatRupiah(angka) {
+                    let num = parseInt(angka);
+                    if (isNaN(num) || num < 0) num = 0;
+                    return new Intl.NumberFormat('id-ID', {
+                        style: 'currency',
+                        currency: 'IDR',
+                        minimumFractionDigits: 0
+                    }).format(num);
+                },
+                initSelect2() {
+                    $('.parcel-select').select2({
+                        placeholder: 'Pilih Parcel',
+                        ajax: {
+                            url: '/ajax/listProduct',
+                            dataType: 'json',
+                            delay: 250,
+                            processResults: function(data) {
+                                return {
+                                    results: data.map(item => ({
+                                        id: item.id,
+                                        text: item.name,
+                                        unit: item.unit,
+                                        price: item.price,
+                                        hpp: item.hpp,
+                                    }))
+                                };
+                            },
+                            cache: true
+                        }
+                    }).on('select2:select', (e) => {
+                        let index = $(e.target).data('index');
+                        let data = e.params.data;
+                        this.parcels[index].product = data.id;
+                        this.parcels[index].name = data.text;
+                        this.parcels[index].unit = data.unit;
+                        this.parcels[index].priceAwal = data.price;
+                        this.parcels[index].hpp = data.hpp;
+                        this.parcels[index].price = data.price;
+                        this.parcels[index].priceFormatted = this.formatRupiah(data.price);
+                        this.parcels[index].qty = 1;
+                    });
+                },
+                saveParcelToCart() {
+                    const budget = document.getElementById('parcel_budget').value;
+                    const qty = document.getElementById('parcel_qty').value;
+                    console.log('Saving parcel to cart:', this.parcels);
+                    this.parcels.forEach(item => {
+                        console.log("Produk ID:", item.product);
+                        console.log("Nama:", item.name);
+                        console.log("Unit:", item.unit.name);
+                        console.log("Harga Awal:", item.priceAwal);
+                        console.log("Qty:", item.qty);
+                    });
+                    // let budgetClean = parseInt(this.budgetParcel.replace(/[^0-9]/g, '')) || 0;
+
+                    // posApp.cart.push({
+                    //     id: Date.now(), // ID unik
+                    //     name: 'Parcel',
+                    //     qty: this.qtyParcel,
+                    //     budget: budgetClean
+                    // });
+
+                    // this.closeAddModal();
+                },
+            }
+        }
+
+        document.addEventListener('alpine:init', () => {
+            window.mainCartInstance = Alpine.data('posApp', posApp);
+            window.parcelFormInstance = Alpine.data('parcelForm', parcelForm);
+        });
     </script>
 @endsection
--- Modules/Pos/Resources/views/pos/segment/modal-parcel.blade.php
+++ Modules/Pos/Resources/views/pos/segment/modal-parcel.blade.php
@@ -1,6 +1,6 @@
 {{-- Modal Add Parcel --}}
 <div class="modal fade" id="parcelModal" tabindex="-1" aria-labelledby="parcelModalLabel" aria-hidden="true"
-    x-show="showParcelModal" style="display: none;">
+    x-show="showParcelModal" style="display: none;" x-data="parcelForm()" x-init="initSelect2">
     <div class="modal-dialog modal-fullscreen-sm-down">
         <div class="modal-content" x-data>
             <div class="modal-header" style="background-color: #ff000d; color: #fff;">
@@ -9,11 +9,63 @@
             </div>
             <div class="modal-body">
                 <!-- Select Produk -->
-                <div class="mb-3">
-                    <label class="form-label">Nama Produk</label>
-                    <select id="select_gift" class="form-select"></select>
-                </div>
+                <div class="row">
+                    <div class="col-3 mb-3">
+                        <label class="form-label">Qty</label>
+                        <input type="number" class="form-control" id="parcel_qty" name="parcel_qty" placeholder="1"
+                            min="1" value="1">
+                    </div>
 
+                    <!-- Harga -->
+                    <div class="col-9 mb-3">
+                        <label class="form-label">Budget</label>
+                        <input type="text" class="form-control format-number" id="parcel_budget" name="parcel_budget"
+                            placeholder="Masukkan budget">
+                    </div>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Pilih Kemasan</label>
+                    <select id="select_kemasan" class="form-select"></select>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Biaya Jasa</label>
+                    <input type="text" class="form-control format-number" id="parcel_jasa" name="parcel_jasa"
+                        placeholder="Masukkan Biaya jasa">
+                </div>
+                <hr>
+
+                <div>
+                    <template x-for="(item, index) in parcels" :key="index">
+                        <div class="border rounded p-3 mb-3">
+                            <div class="row">
+                                <div class="col-9 mb-3">
+                                    <label class="form-label">Parcel</label>
+                                    <select class="form-select parcel-select" :data-index="index"></select>
+                                </div>
+                                <div class="col-3 mb-3">
+                                    <label class="form-label">Qty</label>
+                                    <input type="number" class="form-control" min="1" x-model.number="item.qty">
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Harga Jual (Rp)</label>
+                                <input type="text" class="form-control" x-model="item.priceFormatted"
+                                    @input="updatePrice(index)" inputmode="numeric">
+                            </div>
+                            <button type="button" class="btn btn-danger btn-sm"
+                                @click="removeParcel(index)">Hapus</button>
+                        </div>
+                    </template>
+
+                    <button type="button" class="btn btn-primary mt-2" @click="addParcel()">+ Tambah Parcel</button>
+
+                    <template x-if="parcels.length > 0">
+                        <div>
+                            <hr>
+                            <h5>Total Keseluruhan: <span x-text="formatRupiah(totalAll)"></span></h5>
+                        </div>
+                    </template>
+                </div>
             </div>
             <div class="modal-footer">
                 <button class="btn btn-secondary" @click="closeParcelModal()">Tutup</button>
