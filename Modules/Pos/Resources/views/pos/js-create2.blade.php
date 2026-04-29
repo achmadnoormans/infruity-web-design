a979472c45f61b05c7421f90f8a066869fd937f1
DESKTOP-OVTLN43\USER <irsyad7798@gmail.com>
2026-04-30 05:40:23 +0700

    fix(Pos): fix pos create, modal static

--- Modules/Pos/Resources/views/pos/js-create2.blade.php
+++ Modules/Pos/Resources/views/pos/js-create2.blade.php
@@ -5,6 +5,13 @@
     @include('pos::pos.js.js-parcel')
     @include('pos::pos.js.js-jus')
     <script type="text/javascript">
+        document.addEventListener('DOMContentLoaded', () => {
+            if (window.bootstrap && bootstrap.Modal && bootstrap.Modal.Default) {
+                bootstrap.Modal.Default.backdrop = 'static';
+                bootstrap.Modal.Default.keyboard = false;
+            }
+        });
+
         document.addEventListener('alpine:init', () => {
             window.mainCartInstance = Alpine.data('posApp', posApp);
             window.parcelFormInstance = Alpine.data('parcelForm', parcelForm);
